d5cae4ce70957eeb8fa3e8e6d0ea272d04035b96
Ryan Ketzner <rketzner@knights.ucf.edu>
2023-07-13 09:27:03 -0700

    Added test to GridAlignedBox to ensure reproducibility with seed.

--- GTE/Test/Mathematics/GridAlignedBox_Test.cpp
+++ GTE/Test/Mathematics/GridAlignedBox_Test.cpp
@@ -5,6 +5,21 @@
 
 using namespace gte;
 
+// Test that the same grid is produced when a seed is given as input
+TEST(TestGridAlignedBox, TestSeed)
+{
+    unsigned int seed = 1;
+    Vector3<double> min{-1.0,-1.0,-1.0};
+    Vector3<double> max{-1.0,-1.0,-1.0};
+    AlignedBox3<double> box{min,max};
+
+    int num = 1000;
+    std::vector<Vector3<double>> points_1 = RandomGrid(num, box, seed);
+    std::vector<Vector3<double>> points_2 = RandomGrid(num, box, seed);
+
+    EXPECT_EQ(points_1,points_2);
+}
+
 TEST(TestGridAlignedBox, TestRandomGrid_3D)
 {
     unsigned int seed = 1;
