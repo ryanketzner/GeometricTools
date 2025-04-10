2edb79ffbd2297ae7ca306853422c7f02a26d3a1
Ryan Ketzner <ketzner@ucf.edu>
2025-04-10 16:59:35 -0400

    Replace pow function call with shift to fix compile issue on clang.

--- GTE/Mathematics/ContCone.h
+++ GTE/Mathematics/ContCone.h
@@ -25,7 +25,7 @@ namespace gte
     template <int32_t N, typename Real>
     bool InContainer(AlignedBox<N, Real> const& box, Cone<N, Real> const& cone)
     {
-        constexpr int32_t arraySize = std::pow(2, N);
+        constexpr int32_t arraySize = 1 << N;
         std::array<Vector<N,Real>, arraySize> vertices;
         box.GetVertices(vertices);
 
