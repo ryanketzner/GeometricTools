86fbfa319f0fcc0f2684a31bf0ce508802eda807
Ryan Ketzner <ketzner@ucf.edu>
2025-07-08 10:07:00 -0400

    Updated SDistPointCone to new function. Not sure how it works but passes test.

--- GTE/Mathematics/SDistPointCone.h
+++ GTE/Mathematics/SDistPointCone.h
@@ -22,18 +22,36 @@ namespace gte
             T sDistance;
         };
 
-        Result operator()(Vector<N,T> const& point, Cone<N,T> const& cone, bool robust = false)
+        // Old code
+        // Result operator()(Vector<N,T> const& point, Cone<N,T> const& cone, bool robust = false)
+        // {
+        //     Result result{};
+
+		// 	Vector<N,T> diff = point - cone.ray.origin;
+		// 	T norm_diff = Length(diff, robust);
+			
+		// 	T phi = acos(Dot(cone.ray.direction, diff)/norm_diff) - cone.angle;
+			
+		// 	result.sDistance = norm_diff * sin(phi);
+			
+		// 	return result;
+        // }
+
+        // I think this is equivalent to the old code above
+        Result operator()(Vector<N,T> const& point,
+                  Cone<N,T> const& cone,
+                  bool robust = false)
         {
             Result result{};
 
-			Vector<N,T> diff = point - cone.ray.origin;
-			T norm_diff = Length(diff, robust);
-			
-			T phi = acos(Dot(cone.ray.direction, diff)/norm_diff) - cone.angle;
-			
-			result.sDistance = norm_diff * sin(phi);
-			
-			return result;
+            Vector<N,T> diff = point - cone.ray.origin;
+            auto d = cone.ray.direction;   // assumed unit-length
+            // ← one‐liner, no trig:
+            T sd = Length(diff - (Dot(diff,d))*d, robust)*cone.cosAngle
+                 - Dot(diff,d)*cone.sinAngle;
+
+            result.sDistance = sd;
+            return result;
         }
     };
 }
